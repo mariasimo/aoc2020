fcc445722cc436d7a2f8f275c8f170350f12e8e4
María Simó <maria.simo@secuoyas.com>
2020-12-13 23:49:37 +0100

    solve day07 part 1

--- src/day07/index.js
+++ src/day07/index.js
@@ -1,5 +1,52 @@
-function foo(bool) {
-  return bool
+const capitalize = s => {
+  if (typeof s !== "string") return ""
+  return s.charAt(0).toUpperCase() + s.slice(1)
 }
 
-module.exports = { foo }
+const parseListToObject = input => {
+  const obj = {}
+  input.forEach(el => {
+    const arr = el.split(" ")
+    const outerBg =
+      arr[0] +
+      arr[1]
+        .split("")
+        .map((el, i) => (i === 0 ? el.toLocaleUpperCase() : el))
+        .join("")
+
+    arr.splice(0, 4)
+    const contents = arr.filter(el => !el.includes("bag"))
+
+    const contentsObj = contents.reduce(function (acc, cu, i) {
+      if (i % 3 == 1) {
+        const camelCaseColor = cu + capitalize(contents[i + 1])
+        acc[camelCaseColor] = contents[i - 1]
+      }
+      return acc
+    }, {})
+
+    obj[outerBg] = contentsObj
+  })
+  return obj
+}
+
+// si la bolsa exterior ya contiene una de la baglist, no volver a contar
+function findBagsContaining(bagsObj, bagsToFind, alreadyCounted = []) {
+  let bagList = [...bagsToFind]
+
+  bagList = [...Object.keys(bagsObj)].flatMap((outerBag, i) => {
+    let found = false
+    return bagList.reduce((acc, innerBag) => {
+      found = Object.values(bagsObj)[i].hasOwnProperty(innerBag)
+      if (found && !acc.includes(outerBag)) acc.push(outerBag)
+      return acc
+    }, [])
+  })
+
+  alreadyCounted.push(...bagList)
+  return bagList.length <= 0
+    ? [...new Set(alreadyCounted)].length
+    : findBagsContaining(bagsObj, bagList, alreadyCounted)
+}
+
+module.exports = { capitalize, findBagsContaining, parseListToObject }
--- src/day07/index.test.js
+++ src/day07/index.test.js
@@ -1,14 +1,38 @@
-const { foo } = require("./index")
+const { parseListToObject, capitalize, findBagsContaining } = require("./index")
+const { readInputAsArray } = require("../utils")
 
-describe("Day x: ...", () => {
+const input = readInputAsArray("day07", true)
+const bags = parseListToObject(input)
+
+const testInput = [
+  "light red bags contain 1 bright white bag, 2 muted yellow bags.",
+  "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
+]
+
+const expectedObj = {
+  lightRed: {
+    brightWhite: "1",
+    mutedYellow: "2",
+  },
+  darkOrange: {
+    brightWhite: "3",
+    mutedYellow: "4",
+  },
+}
+
+describe("Day 07", () => {
   describe("Part 1", () => {
-    test("should...", () => {
-      expect(foo(true)).toBe(true)
+    test("capitalize should return string with first letter uppercased", () => {
+      expect(capitalize("test")[0]).toBe("T")
     })
-  })
-  describe("Part 2", () => {
-    test("should...", () => {
-      expect(foo(false)).toBe(false)
+    test("parseListToObject should return input parse correcty as object", () => {
+      expect(parseListToObject(testInput)).toMatchObject(expectedObj)
+    })
+    test("findBagsContaining should return a number", () => {
+      expect(typeof findBagsContaining(bags, ["shinyGold"])).toBe("number")
+    })
+    test("findBagsContaining should return expected result", () => {
+      expect(findBagsContaining(bags, ["shinyGold"])).toBe(4)
     })
   })
 })
